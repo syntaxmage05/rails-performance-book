123d0844e8f0379c3e1807462a94e8150c5e9517
syntaxmage05 <muturinjonge09@gmail.com>
2026-06-01 05:44:02 +0300

    Preloading data with activerecord

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,23 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2022_08_19_212767) do
-  create_table "customer_stats_profiles", charset: "utf8mb3", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "customer_stats_profiles", force: :cascade do |t|
     t.integer "customer_id"
     t.text "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "customers", charset: "utf8mb3", force: :cascade do |t|
+  create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "films", charset: "utf8mb3", force: :cascade do |t|
+  create_table "films", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -32,27 +35,27 @@ ActiveRecord::Schema[8.0].define(version: 2022_08_19_212767) do
     t.text "big_text_column"
   end
 
-  create_table "followings", charset: "utf8mb3", force: :cascade do |t|
+  create_table "followings", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "inventories", charset: "utf8mb3", force: :cascade do |t|
+  create_table "inventories", force: :cascade do |t|
     t.integer "film_id"
     t.integer "store_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "languages", charset: "utf8mb3", force: :cascade do |t|
+  create_table "languages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "rentals", charset: "utf8mb3", force: :cascade do |t|
+  create_table "rentals", force: :cascade do |t|
     t.integer "inventory_id"
     t.integer "customer_id"
     t.datetime "rental_date"
@@ -61,13 +64,13 @@ ActiveRecord::Schema[8.0].define(version: 2022_08_19_212767) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "stores", charset: "utf8mb3", force: :cascade do |t|
+  create_table "stores", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
